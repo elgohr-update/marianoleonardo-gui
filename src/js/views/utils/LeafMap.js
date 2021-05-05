147eaa89aa198eb0a317c553f7aededa57e4e4bf
Francisco Angelo Cabelo <fangelo@cpqd.com.br>
2021-05-05 13:29:42 -0300

    [feat] - Extends leafmap with socket to be used in device's map

--- src/js/views/utils/LeafMap.js
+++ src/js/views/utils/LeafMap.js
@@ -67,14 +67,20 @@ const MarkerUpdater = ({
         axios.get(URL, { headers: { Authorization: `Bearer ${userToken}` } }).then(({ data }) => {
             setSocketInstance(socketio(baseURL, { query: `token=${data.token}`, transports: ['polling'] }));
         }).catch((error) => {
+            // @TODO We should better handle these errors
+            // eslint-disable-next-line no-console
             console.error(error);
         });
     }, []);
 
     useEffect(() => {
         if (socketInstance) {
-            socketInstance.on(id, (data) => handlePosition(data));
-            socketInstance.on('error', (data) => { console.error('Websocket error: ', data); });
+            socketInstance.on(id, data => handlePosition(data));
+            socketInstance.on('error', (data) => {
+                // @TODO We should better handle these errors
+                // eslint-disable-next-line no-console
+                console.error('Websocket error: ', data);
+            });
         }
         // Destroy the socket instance when dismounting the component
         return () => (socketInstance ? socketInstance.close() : undefined);
